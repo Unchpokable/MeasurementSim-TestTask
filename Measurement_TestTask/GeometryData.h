d0a24df9ea9316eeb77ec5252e090c7020de2e13
Unchpokable <Unchpokable.official@gmail.com>
2023-11-05 02:10:49 +0500

    Struct that contains some info about 3d object in space

---
+++ Measurement_TestTask/GeometryData.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <eigen/include/Eigen/Core>
+
+using Point3d = Eigen::Vector3d;
+
+struct Data3D
+{
+    Eigen::Vector3d Normal;
+    Point3d NominalPosition;
+    Point3d ActualPosition;
+};
