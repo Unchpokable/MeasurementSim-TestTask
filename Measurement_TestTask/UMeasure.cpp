a1b27f9562e787e923bcc15b48ac81fb32fda25d
Unchpokable <Unchpokable.official@gmail.com>
2023-11-05 05:13:18 +0500

    AddPoint command added

--- Measurement_TestTask/UMeasure.cpp
+++ Measurement_TestTask/UMeasure.cpp
@@ -2,6 +2,8 @@
 #include <QScrollBar>
 
 #include "AddMoveCommand.h"
+#include "AddPointCommand.h"
+
 
 UMeasure::UMeasure(QWidget *parent)
     : QMainWindow(parent)
@@ -12,6 +14,10 @@ UMeasure::UMeasure(QWidget *parent)
     
     connect(ui->addMoveButton, &QPushButton::clicked, this, [this]() {
         ShowAddCommandForm(ci_move);
+    });
+
+    connect(ui->addPointButton, &QPushButton::clicked, this, [this]() {
+        ShowAddCommandForm(ci_point);
     });
 }
 
@@ -26,14 +32,13 @@ void UMeasure::ShowAddCommandForm(ContextObjectType cmd_type)
     {
     case ci_move:
         {
-            const auto form = new AddMoveCommand(this, 
-                const_cast<RuntimeContext*>(m_interpreter->GetContext()));
-            int ret = form->exec();
-            if (form)
-            {
-                const auto new_command = form->GetConstructedObject();
-                m_interpreter->AddCommand(const_cast<BoundCommand*>(new_command));
-            }
+            ShowFormAndAddCreatedCommand<AddMoveCommand>();
+            return;
+        }
+    case ci_point:
+        {
+            ShowFormAndAddCreatedCommand<AddPointCommand>();
+            return;
         }
     }
 }
