dde665f35cbbfa546662010006b595cc3927403f
Unchpokable <Unchpokable.official@gmail.com>
2023-11-06 23:23:06 +0500

    Fixed constructor parameter type

--- Measurement_TestTask/ReportPointCommand.h
+++ Measurement_TestTask/ReportPointCommand.h
@@ -5,14 +5,16 @@
 class ReportPointCommand : public CommandBase, public ContextObject
 {
 public:
-    ReportPointCommand(QString name, double max_diff)
+    ReportPointCommand(const QString& name, double max_diff)
         : ContextObject(name, ci_point_diff)
     {
         m_maximum_diff = max_diff;
+        m_measured_diff = -static_cast<double>(INFINITY);
     }
 
     void Execute(const std::vector<ContextObject*>& args) override;
     QString ToString() override;
+
     bool IsSatisfyingCondition() const noexcept;
 
 private:
