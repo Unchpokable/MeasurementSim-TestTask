a1af5f325cf383bf0e213e589119f3d684b158d7
Unchpokable <Unchpokable.official@gmail.com>
2023-11-06 22:14:56 +0500

    Point3dEx struct that provides Point's normal, ProjectionPoint3d struct that provides actual and nominal point projections and its normals

--- Measurement_TestTask/GeometryData.h
+++ Measurement_TestTask/GeometryData.h
@@ -7,6 +7,22 @@ using Point3d = Eigen::Vector3d;
 struct Data3D
 {
     Eigen::Vector3d Normal;
+    Eigen::Vector3d ActualNormal;
     Point3d NominalPosition;
     Point3d ActualPosition;
+};
+
+struct Point3dEx
+{
+    Point3d Point;
+    Eigen::Vector3d Normal;
+};
+
+struct ProjectionPoint3d
+{
+    Point3d ProjectionActual;
+    Point3d ProjectionNominal;
+
+    Eigen::Vector3d NormalActual;
+    Eigen::Vector3d NormalNominal;
 };
