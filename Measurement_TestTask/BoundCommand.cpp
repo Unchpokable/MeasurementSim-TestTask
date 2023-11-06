5889e329fc9dfe849078f81de488bb833919de03
Unchpokable <Unchpokable.official@gmail.com>
2023-11-06 05:08:34 +0500

    fixed switch-case

--- Measurement_TestTask/BoundCommand.cpp
+++ Measurement_TestTask/BoundCommand.cpp
@@ -7,15 +7,20 @@ void BoundCommand::Execute()
     case ci_point:
     case ci_move:
     case ci_comment:
-        m_command->Execute(m_args);
+        {
+            m_command->Execute(m_args);
+            return;
+        }
     case ci_circle:
     case ci_plane:
     case ci_project:
     case ci_circle_diff:
     case ci_circle_to_point:
     case ci_point_diff:
-        m_command->Execute(m_alt_args);
-
+        {
+            m_command->Execute(m_alt_args);
+            return;
+        }
     case ci_undefined:
         break;
     }
