4e9f2d49c851e39128117ad5d213b5c68c9b5663
Unchpokable <Unchpokable.official@gmail.com>
2023-11-11 06:42:26 +0500

    Context menu for commands with some commands

--- Measurement_TestTask/UMeasure.cpp
+++ Measurement_TestTask/UMeasure.cpp
@@ -68,6 +68,8 @@ UMeasure::UMeasure(QWidget *parent)
     m_commands_list = new CommandListModel(this);
 
     ui->programView->setModel(m_commands_list);
+
+    AddProgramViewContextMenu();
 }
 
 UMeasure::~UMeasure()
@@ -76,59 +78,119 @@ UMeasure::~UMeasure()
     delete m_interpreter;
 }
 
-void UMeasure::ShowAddCommandForm(ContextObjectType cmd_type)
+void UMeasure::AddProgramViewContextMenu() noexcept
 {
-    switch (cmd_type)
+    contextMenu = new QMenu();
+
+    const auto deleteAction = new QAction("Remove", this);
+    connect(deleteAction, &QAction::triggered, this, &UMeasure::OnDeleteAction);
+    contextMenu->addAction(deleteAction);
+
+    const auto replaceMenu = new QMenu("Replace", this);
+    contextMenu->addMenu(replaceMenu);
+
+    const auto insertAfter = new QMenu("Insert new command after", this);
+    const auto insertBefore = new QMenu("Insert new command before", this);
+
+    contextMenu->addMenu(insertAfter);
+    contextMenu->addMenu(insertBefore);
+
+    ui->programView->setContextMenuPolicy(Qt::CustomContextMenu);
+
+    //TODO: Ёто пиз*ец. пиз*ец должен быть решен.  ак нибудь.
+    AddInsertActionToMenu(replaceMenu, "Comment", ci_comment, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Move", ci_move, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Point", ci_point, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Plane", ci_plane, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Circle", ci_circle, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "CircleCenter", ci_circle_to_point, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Projection", ci_project, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Report Point", ci_point_diff, ip_replace);
+    AddInsertActionToMenu(replaceMenu, "Report Circle", ci_circle_diff, ip_replace);
+
+    AddInsertActionToMenu(insertAfter, "Comment", ci_comment, ip_after);
+    AddInsertActionToMenu(insertAfter, "Move", ci_move, ip_after);
+    AddInsertActionToMenu(insertAfter, "Point", ci_point, ip_after);
+    AddInsertActionToMenu(insertAfter, "Plane", ci_plane, ip_after);
+    AddInsertActionToMenu(insertAfter, "Circle", ci_circle, ip_after);
+    AddInsertActionToMenu(insertAfter, "CircleCenter", ci_circle_to_point, ip_after);
+    AddInsertActionToMenu(insertAfter, "Projection", ci_project, ip_after);
+    AddInsertActionToMenu(insertAfter, "Report Point", ci_point_diff, ip_after);
+    AddInsertActionToMenu(insertAfter, "Report Circle", ci_circle_diff, ip_after);
+
+
+    AddInsertActionToMenu(insertBefore, "Comment", ci_comment, ip_before);
+    AddInsertActionToMenu(insertBefore, "Move", ci_move, ip_before);
+    AddInsertActionToMenu(insertBefore, "Point", ci_point, ip_before);
+    AddInsertActionToMenu(insertBefore, "Plane", ci_plane, ip_before);
+    AddInsertActionToMenu(insertBefore, "Circle", ci_circle, ip_before);
+    AddInsertActionToMenu(insertBefore, "CircleCenter", ci_circle_to_point, ip_before);
+    AddInsertActionToMenu(insertBefore, "Projection", ci_project, ip_before);
+    AddInsertActionToMenu(insertBefore, "Report Point", ci_point_diff, ip_before);
+    AddInsertActionToMenu(insertBefore, "Report Circle", ci_circle_diff, ip_before);
+
+    connect(ui->programView, &QListView::customContextMenuRequested, this, &UMeasure::OnCustomContextMenuRequested);
+}
+
+BoundCommand* UMeasure::GetCommandByType(ContextObjectType type)
+{
+    switch(type)
     {
-    case ci_comment:
+        case ci_comment:
         {
-            ShowFormAndAddCreatedCommand<AddCommentCommand>();
-            return;
+            return ShowFormCreateCommand<AddCommentCommand>();
         }
-    case ci_move:
+        case ci_move:
         {
-            ShowFormAndAddCreatedCommand<AddMoveCommand>();
-            return;
+            return ShowFormCreateCommand<AddMoveCommand>();
+
         }
-    case ci_point:
+        case ci_point:
         {
-            ShowFormAndAddCreatedCommand<AddPointCommand>();
-            return;
+            return ShowFormCreateCommand<AddPointCommand>();
+
         }
-    case ci_plane:
+        case ci_plane:
         {
-            ShowFormAndAddCreatedCommand<AddPlaneCommand>();
-            return;
+            return ShowFormCreateCommand<AddPlaneCommand>();
+
         }
-    case ci_circle:
+        case ci_circle:
         {
-            ShowFormAndAddCreatedCommand<AddCircleCommand>();
-            return;
+            return ShowFormCreateCommand<AddCircleCommand>();
+
         }
-    case ci_circle_to_point:
+        case ci_circle_to_point:
         {
-            ShowFormAndAddCreatedCommand<AddCircleCenterCommand>();
-            return;
+            return ShowFormCreateCommand<AddCircleCenterCommand>();
+
         }
-    case ci_project:
+        case ci_project:
         {
-            ShowFormAndAddCreatedCommand<AddProjectCommand>();
-            return;
+            return ShowFormCreateCommand<AddProjectCommand>();
+
         }
-    case ci_point_diff:
+        case ci_point_diff:
         {
-            ShowFormAndAddCreatedCommand<AddPointDifferenceCommand>();
-            return;
+            return ShowFormCreateCommand<AddPointDifferenceCommand>();
+
         }
-    case ci_circle_diff:
+        case ci_circle_diff:
         {
-            ShowFormAndAddCreatedCommand<AddCircleDifferenceCommand>();
-            return;
+            return ShowFormCreateCommand<AddCircleDifferenceCommand>();
+
         }
 
-    case ci_undefined:
-        return;
+        case ci_undefined:
+            return nullptr;
     }
+    return nullptr;
+}
+
+
+void UMeasure::ShowAddCommandForm(ContextObjectType cmd_type)
+{
+    AddCommand(GetCommandByType(cmd_type));
 }
 
 void UMeasure::ExecuteCommandSequence() noexcept
@@ -144,9 +206,138 @@ void UMeasure::ExecuteCommandSequence() noexcept
     const_cast<std::thread*>(exec_thread)->join();
 }
 
+void UMeasure::AddCommand(BoundCommand* cmd)
+{
+    if(cmd == nullptr)
+        return;
+    m_interpreter->AddCommand(cmd);
+    cmd->Execute();
+    m_commands_list->addObject(cmd);
+    
+}
+
+void UMeasure::ReplaceCommand(BoundCommand* cmd, std::size_t position)
+{
+    if(position < 0 || position >= m_interpreter->GetCommandsCount())
+        throw std::invalid_argument("Index was out of bound");
+
+    if (!m_interpreter->ReplaceCommand(cmd, position))
+    {
+        QMessageBox::warning(this, "Error!", "Unable to replace command");
+        return;
+    }
+    ui->programView->update();
+}
+
+void UMeasure::RemoveCommand(std::size_t position)
+{
+    m_interpreter->RemoveCommand(position);
+    ui->programView->update();
+}
+
+
+
 void UMeasure::OnOutputReceived(const QString& what) const noexcept
 {
     ui->programTextView->append(what);
 }
 
+void UMeasure::OnDeleteAction()
+{
+    const auto index = ui->programView->currentIndex();
+    const auto row = index.row();
+
+    if (!m_interpreter->RemoveCommand(row))
+    {
+        QMessageBox::warning(this, "Error!", "Unable to remove selected command");
+        return;
+    }
+
+    m_commands_list->removeObject(row);
+}
+
+void UMeasure::OnReplaceAction(ContextObjectType type)
+{
+    const auto index = ui->programView->currentIndex();
+    const auto row = index.row();
+
+    const auto command = GetCommandByType(type);
+    command->Execute();
+    if (!m_interpreter->ReplaceCommand(command, row))
+    {
+        QMessageBox::warning(this, "Error!", "Error while replacing command");
+        return;
+    }
+
+    m_commands_list->replaceObject(row, command);
+}
+
+void UMeasure::AddInsertActionToMenu(QMenu* menu, const QString& title, ContextObjectType type, InsertionPosition pos)
+{
+    const auto action = new QAction(title, this);
+
+    switch (pos)
+    {
+    case ip_after:
+        {
+            connect(action, &QAction::triggered, [this, type]() {
+                InsertNewCommandAfter(type);
+            });
+            break;
+        }
+    case ip_before:
+        {
+            connect(action, &QAction::triggered, [this, type]() {
+                InsertNewCommandBefore(type);
+            });
+            break;
+        }
+
+    case ip_replace:
+        {
+            connect(action, &QAction::triggered, this, [this, type]() {
+                OnReplaceAction(type);
+            });
+            break;
+        }
+    }
+
+    connect(action, &QAction::triggered, [this, type]() {
+
+    });
+
+    menu->addAction(action);
+}
+
+void UMeasure::InsertNewCommandBefore(ContextObjectType type)
+{
+    const auto index = ui->programView->currentIndex();
+    int row = 0;
+    if (index.row() > 0)
+    {
+        row = index.row() - 1;
+    }
+
+    const auto command = GetCommandByType(type);
+    command->Execute();
+    m_interpreter->InsertCommand(row, command);
+    m_commands_list->insertObject(row, command);
+}
+
+void UMeasure::InsertNewCommandAfter(ContextObjectType type)
+{
+    const auto index = ui->programView->currentIndex();
+    int row = index.row() - 1;
+
+    if(index.row() < m_interpreter->GetCommandsCount()) 
+    {
+        row = index.row() + 1;
+    }
+
+    const auto command = GetCommandByType(type);
+
+    command->Execute();
+    m_interpreter->InsertCommand(row, command);
+    m_commands_list->insertObject(row, command);
+}
 
--- Measurement_TestTask/UMeasure.h
+++ Measurement_TestTask/UMeasure.h
@@ -11,6 +11,12 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class UMeasureClass; };
 QT_END_NAMESPACE
 
+enum InsertionPosition {
+    ip_before,
+    ip_after,
+    ip_replace
+};
+
 class UMeasure : public QMainWindow
 {
     Q_OBJECT
@@ -20,15 +26,26 @@ public:
     ~UMeasure() override;
 
 private:
+    void AddProgramViewContextMenu() noexcept;
+
     void ShowAddCommandForm(ContextObjectType cmd_type);
     void ExecuteCommandSequence() noexcept;
+    void AddCommand(BoundCommand*);
+    void ReplaceCommand(BoundCommand*, std::size_t);
+    void RemoveCommand(std::size_t);
+    void AddInsertActionToMenu(QMenu* menu, const QString& title, ContextObjectType type, InsertionPosition pos);
+
+    void InsertNewCommandBefore(ContextObjectType type);
+    void InsertNewCommandAfter(ContextObjectType type);
+
+    BoundCommand* GetCommandByType(ContextObjectType type);
 
     //Если кому то интересно наглядное определение слова "Кодохульство" - то вот.
 
     template<typename TAddCommand,
         typename = std::enable_if_t<std::is_base_of_v<AddCommandBase, TAddCommand> &&
                                     std::is_base_of_v<QDialog, TAddCommand>>>
-    void ShowFormAndAddCreatedCommand()
+    BoundCommand* ShowFormCreateCommand()
     {
         const auto form = new TAddCommand(this, const_cast<RuntimeContext*>(m_interpreter->GetContext()));
         int ret = form->exec();
@@ -38,20 +55,29 @@ private:
             const auto new_command = const_cast<BoundCommand*>(form->GetConstructedObject());
 
             if(new_command == nullptr)
-                return;
-            m_interpreter->AddCommand(new_command);
-            new_command->Execute();
-            m_commands_list->addObject(new_command);
+                return nullptr;
+            return new_command;
         }
+
+        return nullptr;
     }
 
     Ui::UMeasureClass *ui;
     CommandInterpreter* m_interpreter;
     CommandListModel* m_commands_list;
+    QMenu* contextMenu;
 
 signals:
     void OutputReceived(const QString& what);
 
 private slots:
     void OnOutputReceived(const QString& what) const noexcept;
+
+    void OnDeleteAction();
+    void OnReplaceAction(ContextObjectType type);
+
+    inline void OnCustomContextMenuRequested(const QPoint& pos)
+    {
+        contextMenu->exec(ui->programView->mapToGlobal(pos));
+    }
 };
